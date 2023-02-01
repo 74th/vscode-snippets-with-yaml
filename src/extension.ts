9203ae8db57e5b88d70ce622e42fa493184fd227
ahen <10469240+afmhenry@users.noreply.github.com>
2023-02-01 14:57:07 +0100

    works with workspace snippets now...no idea how to progress

--- src/extension.ts
+++ src/extension.ts
@@ -50,12 +50,31 @@ async function getAvailableSnippets(snippetsDir: string): Promise<string[]> {
     }
     const languages: string[] = [];
     for (const file of files) {
-        if (path.extname(file) !== ".json") {
+        if ([".json", ".code-snippets"].includes(path.extname(file))) {
             continue;
         }
-        languages.push(path.basename(file, ".json"));
+        languages.push(path.basename(file, path.extname(file)));
     }
     return languages;
+}
+
+async function getWorkspaceSnippets(snippetsDir: string): Promise<string[]> {
+
+    let files: string[];
+    try {
+        files = await fs.readdir(snippetsDir);
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+    const workspaceSnippets: string[] = [];
+    for (const file of files) {
+        if (![".code-snippets"].includes(path.extname(file))) {
+            continue;
+        }
+        workspaceSnippets.push(path.basename(file, path.extname(file)));
+    }
+    return workspaceSnippets;
 }
 
 interface PickSnippetsItem extends vscode.QuickPickItem {
@@ -110,6 +129,23 @@ async function listSnippetsLanguageItems(snippetsDir: string): Promise<PickSnipp
     return result;
 }
 
+async function listWorkspaceSnippetsItems(snippetsDir: string): Promise<PickSnippetsItem[]> {
+    const result: PickSnippetsItem[] = [];
+
+    const availableList = await getWorkspaceSnippets(snippetsDir);
+    availableList.forEach(workspaceSnippet => {
+        result.push({
+            label: workspaceSnippet.toString(),
+            languageID: "foo",
+            description: "Workspace snippets: " + workspaceSnippet.toString(),
+            available: true,
+            path: path.join(snippetsDir, workspaceSnippet + ".code-snippets"),
+        });
+    })
+
+    return result
+}
+
 async function convertYAMLSnippets(jsonPath: string, available: boolean): Promise<string> {
     const yamlPath = jsonPath + ".yaml";
     let doc: SnippetDocument = {};
@@ -139,8 +175,8 @@ async function convertYAMLSnippets(jsonPath: string, available: boolean): Promis
     return yamlPath;
 }
 
-function isYAMLSnippetsPath(yamlPath: string, snippetsDir: string): boolean {
-    return snippetsDir.toLowerCase() === path.dirname(yamlPath).toLowerCase() && path.basename(yamlPath).endsWith(".json.yaml");
+function isYAMLSnippetsPath(yamlPath: string, snippetsDir: string, useExt: string): boolean {
+    return snippetsDir.toLowerCase() === path.dirname(yamlPath).toLowerCase() && path.basename(yamlPath).endsWith(useExt + ".yaml");
 }
 
 async function convertJSONSnippets(yamlPath: string) {
@@ -160,8 +196,8 @@ async function convertJSONSnippets(yamlPath: string) {
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-
     const snippetsDir = getSnippetsDir();
+    const userOrWorkspace = { "user": { "ext": ".json", "path": snippetsDir }, "workspace": {} }
 
     let disposable = vscode.commands.registerCommand('editing-snippets-by-yaml.configureUserSnippets', async () => {
         const items = await listSnippetsLanguageItems(snippetsDir);
@@ -173,35 +209,56 @@ export async function activate(context: vscode.ExtensionContext) {
         await vscode.commands.executeCommand("vscode.open", vscode.Uri.file(yamlPath));
 
     });
-    context.subscriptions.push(disposable);
 
-    disposable = vscode.workspace.onDidCloseTextDocument(async (doc) => {
-        const yamlPath = doc.fileName;
-        if (!isYAMLSnippetsPath(yamlPath, snippetsDir)) {
-            return;
-        }
-        try {
-            await convertJSONSnippets(yamlPath);
-        } catch (e) {
-            vscode.window.showErrorMessage(`cannot create json : ${e.message}`);
-        }
-        fs.unlink(yamlPath);
-    });
     context.subscriptions.push(disposable);
+    if (vscode.workspace.workspaceFolders) {
+        const workspaceSnippets = vscode.workspace.workspaceFolders[0].uri.fsPath + "\\.vscode";
+        userOrWorkspace["workspace"] = { "ext": ".code-snippets", "path": workspaceSnippets }
 
-    disposable = vscode.workspace.onDidSaveTextDocument(async (doc) => {
-        const yamlPath = doc.fileName;
-        if (!isYAMLSnippetsPath(yamlPath, snippetsDir)) {
-            return;
-        }
-        try {
-            await convertJSONSnippets(yamlPath);
-        } catch (e) {
-            vscode.window.showErrorMessage(`cannot create json : ${e.message}`);
-        }
-    });
-    context.subscriptions.push(disposable);
+        disposable = vscode.commands.registerCommand('editing-snippets-by-yaml.configureWorkplaceSnippets', async () => {
+            const items = await listWorkspaceSnippetsItems(workspaceSnippets);
+            const selected = await vscode.window.showQuickPick(items);
+            if (!selected) {
+                return;
+            }
+            const yamlPath = await convertYAMLSnippets(selected.path, selected.available);
+            await vscode.commands.executeCommand("vscode.open", vscode.Uri.file(yamlPath));
 
+        });
+        context.subscriptions.push(disposable);
+        disposable = vscode.workspace.onDidCloseTextDocument(async (doc) => {
+            const yamlPath = doc.fileName;
+            const mode = yamlPath.includes(".code-snippets.yaml")
+            const useDirectory = mode ? workspaceSnippets : snippetsDir
+            const useExt = mode ? ".code-snippets" : ".json"
+            if (!isYAMLSnippetsPath(yamlPath, useDirectory, useExt)) {
+                return;
+            }
+            try {
+                await convertJSONSnippets(yamlPath);
+            } catch (e) {
+                vscode.window.showErrorMessage(`cannot create json : ${e.message}`);
+            }
+            fs.unlink(yamlPath);
+        });
+        context.subscriptions.push(disposable);
+
+        disposable = vscode.workspace.onDidSaveTextDocument(async (doc) => {
+            const yamlPath = doc.fileName;
+            const mode = yamlPath.includes(".code-snippets.yaml")
+            const useDirectory = mode ? workspaceSnippets : snippetsDir
+            const useExt = mode ? ".code-snippets" : ".json"
+            if (!isYAMLSnippetsPath(yamlPath, useDirectory, useExt)) {
+                return;
+            }
+            try {
+                await convertJSONSnippets(yamlPath);
+            } catch (e) {
+                vscode.window.showErrorMessage(`cannot create json : ${e.message}`);
+            }
+        });
+        context.subscriptions.push(disposable);
+    }
 }
 
 
